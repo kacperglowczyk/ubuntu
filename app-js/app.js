ce8a85b538715c12000556f4be505f83d27f7734
Lab <glowczykkacper@gmail.com>
2024-10-25 14:23:57 +0200

    New commit

--- app-js/app.js
+++ app-js/app.js
@@ -1,1 +1,8 @@
-console.log("yo yo yo");
+const button = document.getElementById('changeColorButton');
+const body = document.body;
+
+button.addEventListener('click', () => {
+    const randomColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    body.style.backgroundColor = randomColor;
+});
+
